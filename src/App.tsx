121eb4e9b9641537782268cf830aa52abe0404b5
Dany Kamel <danyabdo94@gmail.com>
2023-04-12 21:39:53 +0200

    Add login page with translations

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,35 @@
 import React from 'react';
-
 import './App.css';
+import i18n from "i18next";
+import { initReactI18next, useTranslation } from "react-i18next";
+
+import enTranslationFile from "./translations/en.json"
 import Login from './components/Login';
 
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: {
+        translation: { ...enTranslationFile }
+      }
+    },
+    lng: "en",
+    fallbackLng: "en",
+
+    interpolation: {
+      escapeValue: false
+    }
+  });
+
 function App() {
+  const { t } = useTranslation();
+
   return (
-    <Login />
+    <>
+      <Login />
+      <h1>{t('signin')}</h1>
+    </>
   );
 }
 
